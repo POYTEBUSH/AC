92cc5096e7581c9377f6f536569c5dfa84b6b870
Peter Bush <poytebush@gmail.com>
2019-03-17 14:57:41 +0000

    Keep

---
+++ source/src/pb_marpo.cpp
@@ -0,0 +1,12 @@
+#include "cube.h"
+#include "pb_marpo.h"
+
+
+pb_marpo::pb_marpo()
+{
+}
+
+
+pb_marpo::~pb_marpo()
+{
+}
---
+++ source/src/pb_marpo.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "pb_target.h"
+
+class pb_marpo
+{
+public:
+	pb_marpo();
+	~pb_marpo();
+};
