e6be7b26e73a49fa675a150673742ad684dd5bd8
Peter Bush <poytebush@gmail.com>
2019-03-27 13:23:55 +0000

    Keep - up to date

--- source/src/bot/ac_bot.cpp
+++ source/src/bot/ac_bot.cpp
@@ -62,11 +62,6 @@ void CACBot::Spawn()
 
      m_vGoal = m_vWaterGoal = g_vecZero;
 
-	 //Attach bot to its MARPO Manager
-	 pb_marpomanager::Instance().AddInstance(new pb_marpo(this));
-	 //pb_target_wander* defaultTarget = new pb_target_wander(TASK_LEVEL_LONGTERM);
-	 //mMARPO_Manager->SetDefaultTarget(defaultTarget);
-
      ResetWaypointVars();
 }
 
