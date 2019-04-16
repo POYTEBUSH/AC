27445f508b4611cb3a720b818395f4576f3409bc
STUDENT\b013432f <potyebush@gmail.com>
2019-04-16 13:27:35 +0100

    Added Fuzzification and DeFuzzification of results

--- source/src/bot/pb_FuzzyModule.cpp
+++ source/src/bot/pb_FuzzyModule.cpp
@@ -14,3 +14,30 @@ void pb_FuzzyModule::AddRule(pb_FuzzyTerm & atecedent, pb_FuzzyTerm & concequenc
 {
 	mFuzzyRules.push_back(new pb_FuzzyRule(atecedent, concequence));
 }
+
+pb_FuzzyVariable & pb_FuzzyModule::CreateFuzzyVariable(const std::string & name)
+{
+	mVariables[name] = new pb_FuzzyVariable();
+	return *mVariables[name];
+}
+
+double pb_FuzzyModule::Fuzzify(const std::string & name, double val)
+{
+	mVariables[name]->Fuzzify(val);
+}
+
+double pb_FuzzyModule::DeFuzzify(const std::string & name)
+{
+	assert(mVariables.find(name) != end(mVariables));
+
+	for (auto r : mFuzzyRules)
+	{
+		r->Calculate();
+	}
+
+	return mVariables[name]->DeFuzzify(10);
+}
+
+void pb_FuzzyModule::ZeroConsequence()
+{
+}
--- source/src/bot/pb_botmanager.cpp
+++ source/src/bot/pb_botmanager.cpp
@@ -21,11 +21,29 @@ void pb_botmanager::Init()
 	pb_FzSet armourHigh = botArmour.AddFuzzySet(FuzzySetType::RightShoulder, "High", 30, 50, 100);
 
 	pb_FuzzyVariable& desirabilityScore = mFuzzyModule.CreateFuzzyVariable("Desirability");
-	pb_FzSet desirabilityLow = desirabilityScore.AddFuzzySet(FuzzySetType::LeftShoulder, "Low", 0, 30, 50);
-	pb_FzSet desirabilityNeutral = desirabilityScore.AddFuzzySet(FuzzySetType::Triangular, "Neutral", 30, 50, 70);
-	pb_FzSet desirabilityHigh = desirabilityScore.AddFuzzySet(FuzzySetType::RightShoulder, "High", 50, 70, 100);
+	pb_FzSet desirabilityMinimal = desirabilityScore.AddFuzzySet(FuzzySetType::LeftShoulder, "MinimalDesirability", 0, 25, 30);
+	pb_FzSet desirabilityLow = desirabilityScore.AddFuzzySet(FuzzySetType::Triangular, "LowDesirability", 30, 40, 50);
+	pb_FzSet desirabilityNeutral = desirabilityScore.AddFuzzySet(FuzzySetType::Triangular, "NeutralDesirability", 40, 50, 60);
+	pb_FzSet desirabilityHigh = desirabilityScore.AddFuzzySet(FuzzySetType::Triangular, "HighDesirability", 50, 60, 70);
+	pb_FzSet desirabilityFull = desirabilityScore.AddFuzzySet(FuzzySetType::RightShoulder, "FullDesirability", 70, 75, 100);
 
-	mFuzzyModule.AddRule(pb_FuzzyTermGroup(targetClose, healthLow), desirabilityLow);
+	mFuzzyModule.AddRule(pb_FuzzyTermGroup(targetClose, healthLow, armourLow), desirabilityFull);
+	mFuzzyModule.AddRule(pb_FuzzyTermGroup(targetClose, healthLow), desirabilityFull);
+	mFuzzyModule.AddRule(pb_FuzzyTermGroup(targetClose, armourLow), desirabilityFull);
+	mFuzzyModule.AddRule(pb_FuzzyTermGroup(targetClose, healthMedium), desirabilityFull);
+	mFuzzyModule.AddRule(pb_FuzzyTermGroup(targetClose, healthHigh), desirabilityFull);
+
+	mFuzzyModule.AddRule(pb_FuzzyTermGroup(targetNear, healthLow, armourLow), desirabilityLow);
+	mFuzzyModule.AddRule(pb_FuzzyTermGroup(targetNear, healthLow), desirabilityNeutral);
+	mFuzzyModule.AddRule(pb_FuzzyTermGroup(targetNear, armourLow), desirabilityNeutral);
+	mFuzzyModule.AddRule(pb_FuzzyTermGroup(targetNear, healthMedium), desirabilityHigh);
+	mFuzzyModule.AddRule(pb_FuzzyTermGroup(targetNear, healthHigh), desirabilityFull);
+
+	mFuzzyModule.AddRule(pb_FuzzyTermGroup(targetFar, healthLow, armourLow), desirabilityMinimal);
+	mFuzzyModule.AddRule(pb_FuzzyTermGroup(targetFar, healthLow), desirabilityLow);
+	mFuzzyModule.AddRule(pb_FuzzyTermGroup(targetFar, armourLow), desirabilityLow);
+	mFuzzyModule.AddRule(pb_FuzzyTermGroup(targetFar, healthMedium), desirabilityNeutral);
+	mFuzzyModule.AddRule(pb_FuzzyTermGroup(targetFar, healthHigh), desirabilityNeutral);
 
 
 
